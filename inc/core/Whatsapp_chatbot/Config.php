45f35b59c3992f5578a0c1bcd11747bb861254b7
Abdul Hanan <hananbhatti440@gmail.com>
2023-12-09 02:19:45 +0500

    First Commit

---
+++ inc/core/Whatsapp_chatbot/Config.php
@@ -0,0 +1,22 @@
+<?php return array (
+  'id' => 'whatsapp_chatbot',
+  'folder' => 'core',
+  'name' => 'Chatbot',
+  'author' => 'Stackcode',
+  'author_uri' => 'https://stackposts.com',
+  'desc' => 'Communicate with users',
+  'icon' => 'fad fa-user-robot',
+  'color' => '#d36625',
+  'parent' => 
+  array (
+    'id' => 'features',
+    'name' => 'Features',
+  ),
+  'menu' => 
+  array (
+    'tab' => 2,
+    'type' => 'top',
+    'position' => 1070,
+    'name' => 'WA Chatbot',
+  ),
+);
